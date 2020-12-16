9757152158b7fc9af853558732f15b9001eb5a67
Oluwayomi Oke <okeoluwayomi9@gmail.com>
2020-12-16 15:31:43 +0100

    Update process.php

--- process.php
+++ process.php
@@ -11,5 +11,5 @@
     //send email
     mail($admin_email, "New Form Submission", $message . ' - ' . $phone, "From:" . $email);
     
-    header('Location: http://127.0.0.1:5500/success.html');
+    header('Location: https://oke-yomi.github.io/fictional-financial-institute/success.html');
   }
